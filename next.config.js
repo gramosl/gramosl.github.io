f0e0c824d0d07e7bdd337beef0e2b82d101c907e
Guillermo Ramos <guillermo@kadmos.io>
2023-09-05 19:06:28 +0200

    chore: Add CI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Add basePath
+  basePath: "/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
